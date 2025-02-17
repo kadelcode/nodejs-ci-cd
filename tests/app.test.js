05b9bcb4abcbd7f4d3300a1b8f2a441d16c4c40c
kadelcode <kadelcode@gmail.com>
2025-02-17 12:16:27 +0100

    Update test

--- tests/app.test.js
+++ tests/app.test.js
@@ -5,6 +5,6 @@ describe('GET /', () => {
     it('should return a JSON response', async () => {
         const res = await request(app).get('/');
         expect(res.statusCode).toEqual(200);
-        expect(res.body).toHaveProperty('message', 'Hello, Node.js CI/CD');
+        expect(res.body).toHaveProperty('message', 'Hello, Node.js CI/CD!');
     });
 });
